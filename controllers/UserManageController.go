68a47a869eaadc40c7f7e446fc5b766953dfbe26
LittleRong <947698259@qq.com>
2019-02-17 13:54:09 +0800

    [feat]user manage

--- controllers/UserManageController.go
+++ controllers/UserManageController.go
@@ -1,11 +1,78 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"hello/models/user"
+)
 
 type UserManageController struct{
 	beego.Controller
 }
 
+func (this *UserManageController) UserManageInit(){
+	this.TplName = "manage/user_manage.html"
+}
+
 func (this *UserManageController) UserManage(){
-	this.TplName = "index/user_manage.html"
+	offset,_ := this.GetInt("offset")
+	limit,_ := this.GetInt("limit")
+
+	user_list := user.GetUserListByOffstAndLimit(offset,limit)
+
+	//user_data,page_num
+	beego.Info("======user_list=====",user_list)
+	var result map[string]interface{}
+	result = make(map[string]interface{})
+	result["user_data"] = user_list
+	result["page_num"] = offset
+	this.Data["json"] = result
+	this.ServeJSON()
+	return
+
+}
+
+func (this *UserManageController) ChangeUser(){
+	change_id,_ := this.GetInt("change_id")
+	user_name := this.GetString("user_name")
+	login_name := this.GetString("login_name")
+	user_phone_number := this.GetString("user_phone_number")
+	user_job_number := this.GetString("user_job_number")
+	user_gender,_ := this.GetInt("user_gender")
+
+	r :=user.UpdateUserById (change_id,user_name,login_name,user_phone_number,user_job_number,user_gender)
+	var result map[string]interface{}
+	result = make(map[string]interface{})
+	result["result"] = r
+	this.Data["json"] = result
+	this.ServeJSON()
+	return
+}
+
+func (this *UserManageController) AddUser(){
+	user_name := this.GetString("user_name")
+	login_name := this.GetString("login_name")
+	user_phone_number := this.GetString("user_phone_number")
+	user_job_number := this.GetString("user_job_number")
+	user_gender,_ := this.GetInt("user_gender")
+
+	r :=user.AddUser (user_name,login_name,user_phone_number,user_job_number,user_gender)
+	var result map[string]interface{}
+	result = make(map[string]interface{})
+	result["result"] = r
+	this.Data["json"] = result
+	this.ServeJSON()
+	return
+
+}
+
+func (this *UserManageController) DeleteUser(){
+	delete_id,_ := this.GetInt("delete_id")
+	r :=user.DeleteUserById(delete_id)
+	var result map[string]interface{}
+	result = make(map[string]interface{})
+	result["result"] = r
+	this.Data["json"] = result
+	this.ServeJSON()
+	return
+
 }
--- models/user/user.go
+++ models/user/user.go
@@ -94,3 +94,77 @@ func UpdateUserPwd(user_id int,old_pwd string,pwd string) string {
 	}
 	return "faild"
 }
+
+func GetUserListByOffstAndLimit(offset int,limit int) []User {
+	var u []User
+	o := orm.NewOrm()
+	offset = offset-1
+	o.QueryTable("user").Filter("deleted",0).Offset(offset*limit).Limit(limit).All(&u,"id","login_name","name","phone_number","job_number","gender")
+	beego.Info("======GetUserListByOffstAndLimit=====",u)
+	return u
+}
+
+func UpdateUserById (change_id int,user_name string,login_name string,user_phone_number string,user_job_number string,user_gender int) string {
+	u := User{Id:change_id}
+	o := orm.NewOrm()
+	if o.Read(&u) == nil {
+		u.Name = user_name
+		u.Login_name = login_name
+		u.Phone_number = user_phone_number
+		u.Job_number = user_job_number
+		u.Gender = user_gender
+		if num, err := o.Update(&u); err == nil {
+			beego.Info("======UpdateUserById's num=====",num)
+			return "success"
+		} else if err!=nil{
+			beego.Info("======UpdateUserById's err=====",err)
+			return "update faild"
+		}
+	}
+	return " user doesn't exist "
+}
+
+func AddUser (user_name string,login_name string,user_phone_number string,user_job_number string,user_gender int) string {
+	//login_name不能重复
+	var u User
+	o := orm.NewOrm()
+	o.QueryTable("user").Filter("login_name", login_name).All(&u)
+	if(u != User{}){
+		return "login name have exit,please change another"
+	}
+
+	//user_gender只能是0或1
+	if(user_gender>1||user_gender<0){
+		user_gender = 0
+	}
+
+	u.Name = user_name
+	u.Login_name = login_name
+	u.Phone_number = user_phone_number
+	u.Job_number = user_job_number
+	u.Gender = user_gender
+	id,err := o.Insert(&u)
+	if err == nil {
+		beego.Info("======AddUser's id=====",id)
+		return "success"
+	} else {
+		beego.Info("======AddUser's err=====",err)
+		return "insert faild"
+	}
+}
+
+func DeleteUserById(delete_id int) string{
+	u := User{Id:delete_id}
+	o := orm.NewOrm()
+	if o.Read(&u) == nil {
+		u.Deleted = true
+		if num, err := o.Update(&u); err == nil {
+			beego.Info("======DeleteUserById's num=====",num)
+			return "success"
+		} else if err!=nil{
+			beego.Info("======DeleteUserById's err=====",err)
+			return "delete faild"
+		}
+	}
+	return " user doesn't exist "
+}
